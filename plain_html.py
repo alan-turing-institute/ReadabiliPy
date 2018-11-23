449bf87a20ff3ac1868b87751e6bc64802b598ee
James Robinson <james.em.robinson@gmail.com>
2018-11-23 13:58:12 +0000

    First attempt at bs4-based parsing. Currently tests are failing since parse_to_tree is returning non-prettified output. q, sub and sup tags not yet treated correctly. Double br in bare text is not behaving correctly.

--- plain_html.py
+++ plain_html.py
@@ -4,10 +4,6 @@ import regex
 import unicodedata
 
 
-def string_element_types():
-    return [NavigableString, Comment]
-
-
 def elements_to_delete():
     html5_form_elements = ['button', 'datalist', 'fieldset', 'form', 'input', 'label', 'legend', 'meter', 'optgroup',
                             'option', 'output', 'progress', 'select', 'textarea']
@@ -27,23 +23,30 @@ def elements_to_delete():
     return elements
 
 
+def elements_to_replace_with_contents():
+    """Elements that we will discard while keeping their contents."""
+    elements = ['a', 'abbr', 'address', 'b', 'bdi', 'bdo', 'cite', 'code', 'del', 'dfn', 'em', 'i', 'ins', 'kbs',
+                'mark', 'q', 'rb', 'ruby', 'rp', 'rt', 'rtc', 's', 'samp', 'small', 'span', 'strong', 'u', 'var', 'wbr']
+    return elements
+
+
 def block_level_whitelist():
+    """Elements that we will always accept."""
     elements = ['article', 'aside', 'blockquote', 'caption', 'colgroup', 'col', 'div', 'dl', 'dt', 'dd', 'figure',
                 'figcaption', 'footer', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'header', 'li', 'main', 'ol', 'p', 'pre',
                 'section', 'table', 'tbody', 'thead' 'tfoot', 'tr', 'td', 'th', 'ul']
     return elements
 
+
 def normalise_unicode(text):
-    # Normalise unicode such that things that are visually equivalent map to the same
-    # unicode string where possible
+    """Normalise unicode such that things that are visually equivalent map to the same unicode string where possible."""
     normal_form = "NFKC"
     text = unicodedata.normalize(normal_form, text)
     return text
 
 
 def normalise_whitespace(text):
-    # Replace runs of multiple whitespace characters with a single space as this is what happens when HTML text is
-    # displayed
+    """Replace runs of whitespace characters with a single space as this is what happens when HTML text is displayed."""
     text = regex.sub("\s+", " ", text)
     # Remove leading and trailing whitespace
     text = text.strip()
@@ -51,81 +54,173 @@ def normalise_whitespace(text):
 
 
 def normalise_text(text):
+    """Normalise unicode and whitespace."""
     # Normalise unicode first to try and standardise whitespace characters as much as possible before normalising them
     text = normalise_unicode(text)
     text = normalise_whitespace(text)
     return text
 
 
-def consolidate_child_text(element):
-    prev_child = None
-    for child in element.children:
-        # If both previous and current child are strings, append the current string to the previous string and
-        # remove the current string
-        if prev_child and type(prev_child) is NavigableString and type(child) is NavigableString:
-            text = normalise_text("{prev} {cur}", prev=str(prev_child), cur=str(child))
-            prev_child.replace_with(NavigableString(text))
-            child.extract()
-            # As we've removed the current child, we leave prev_child untouched
-        else:
-            # Do nothing and set current child as previous child for next iteration
-            prev_child = child
-    return
+def remove_blacklist(soup):
+    """Remove all blacklisted elements."""
+    for element_name in elements_to_delete():
+        for element in soup.find_all(element_name):
+            element.decompose()
 
 
-def process_element(element):
-    # Remove elements that we don't want to keep at all
-    if element.name in elements_to_delete():
-        element.extract()
-    # String elements don't have children and are always retained so just normalise the string and return
-    elif type(element) in string_element_types():
-        # Treat Beautiful Soup text elements as strings when normalising as normalisation returns a copy of the string
+def flatten_elements(soup):
+    """Flatten all elements where we are only interested in their contents."""
+    for element_name in elements_to_replace_with_contents():
+        for element in soup.find_all(element_name):
+            element.unwrap()
+
+
+def reformat_linebreaks(soup):
+    """Replace linebreaks with close-parent reopen-parent."""
+
+    BREAK_INDICATOR = "|CLOSE_AND_REOPEN|"
+
+    # Iterate through the tree, removing single <br> and replacingmultiple <br> with BREAK_INDICATOR
+    for element in soup.find_all("br"):
+        # This check is needed since we're modifying the list while iterating through it
+        if element.name == "br":
+            # Start with empty list and populate it with all consecutive <br> elements
+            element_list, current = [], element
+            while current.name == "br":
+                element_list.append(current)
+                current = current.next_sibling
+            # If there's only one <br> then we strip it out
+            if len(element_list) == 1:
+                element_list[0].decompose()
+            # If there are multiple <br>s then we replace them with BREAK_INDICATOR
+            else:
+                element_list[0].replace_with(BREAK_INDICATOR)
+                for element in element_list[1:]:
+                    element.decompose()
+
+    # Iterate through the tree, replacing <hr> with BREAK_INDICATOR
+    for element in soup.find_all("hr"):
+        # This check is needed since we're modifying the list while iterating through it
+        if element.name == "hr":
+            element.replace_with(BREAK_INDICATOR)
+    
+    # print_all(soup)
+
+    # Iterate through the tree again replacing BREAK_INDICATOR with close-parent reopen-parent
+    for element in soup.descendants:
+        element_as_string = str(element)
+
+        # For each element where we find a BREAK_INDICATOR, replace this with a "close-parent reopen-parent" string
+        if BREAK_INDICATOR in element_as_string:
+            multielement_string = element_as_string.replace(BREAK_INDICATOR, "</{0}><{0}>".format(element.name))
+
+            # Now we need to re-parse this string to correctly split it into elements
+            parse_tree = BeautifulSoup(multielement_string, "html.parser")            
+
+            # ... and replace the element with the parsed output
+            element.replace_with(parse_tree)
+
+
+def normalise_strings(soup):
+    # Iterate over all strings in the tree (including bare strings outside tags)
+    for element in soup.find_all(string=True):
+        # Treat Beautiful Soup text elements as strings when normalising since normalisation returns a copy of the string
         text = str(element)
         normalised_text = normalise_text(text)
         # Replace the element with a new string element of the same type, but containing the normalised text
         element.replace_with(type(element)(normalised_text))
-    # Otherwise, process potentially complex element
-    else:
-        # We want to process all the children of the element before we process the element itself.
-        for child in element.children:
-            # Process the child element. Note that processing an element will often replace it with a modified version,
-            # so we can't re-use our references to the child for further processing
-            process_element(child)
-        # NOTE: We query for the elements children again because most children will be replaced when processed above,
-        # so any existing references to the children from before they were processed are likely to be invalid
-        # 1. Consolidate consecutive strings into single strings
-        consolidate_child_text(element)
-        # 2. Replace 'hr' and 'br' defined paragraph breaks with real 'p's (for us do this for even single 'br's
-        #    After string consolidation, we can just wrap the previous and next elements if they are NavigableText and
-        #    then drop the 'br' or 'hr' (NOT QUITE: if parent is 'p' can't just wrap text with p. Need to split 'p'
-        #    into two
-        make_virtual_paragraphs_concrete(element)
-        # One or more consecutive 'hr's or two or more consecutive 'br's indicates a paragraph break. Find any in the
-        # children of this element and ensure the blocks of text that come before and afterwards are in separate
-        # whitelisted block level elements.
 
 
-
-        # After processing each child we want to ensure that all conceptual paragraphs are
-        # Once we have processed all children, process this element
-        # 1. Remove elements that we don't want to keep at all
-        if element.name in elements_to_delete():
+def consolidate_text(soup):
+    # Iterate over all strings in the tree
+    for element in soup.find_all(string=True):
+        # If the previous element is the same type then extract the current string and append to previous
+        if type(element.previous_sibling) is type(element):
+            text = " ".join([str(element.previous_sibling), str(element)])
+            element.previous_sibling.replace_with(text)
             element.extract()
-        # 2. Deal with elements that have special handling
-        # 2a. Replace 'quote' elements with quotation marks. We can safely use getText() as we have already processed all
-        # child elements
-        elif element.name == 'q':
-            element.replace_with("\"{}\"".format(normalise_textelement.getText(" ", strip=True)))
-        # 2b. Replace 'sub' element with '_' prefix
-        elif element.name == 'sub':
-            element.replace_with("_{}".format(normalise_text(element.getText(" ", strip=True))))
-        # 2b. Replace 'sup' element with '^' prefix
-        elif element.name == 'sup':
-            element.replace_with("^{}".format(normalise_text(element.getText(" ", strip=True))))
-        # 3. Replace all other elements with their text contents
-        else:
-            element.replace_with(normalise_text(element.getText(" ", strip=True)))
-        return
-        'br'
-        'hr'
-    return
+
+
+def wrap_bare_text(soup):
+    # Iterate over all strings in the tree
+    for element in soup.find_all(string=True):
+        # Identify any strings whose parent is not a whitelisted element
+        if element.parent.name not in block_level_whitelist():
+            # ... and wrap them in <p> tags
+            p_element = soup.new_tag("p")
+            p_element.string = element
+            element.replace_with(p_element)
+
+# def print_all(soup):
+#     for element in soup.descendants:
+#         print(type(element), element.name, element)
+
+
+def parse_to_tree(html, content_digests=False, node_indexes=False):
+    # Convert the HTML into a Soup parse tree
+    soup = BeautifulSoup(html, "html.parser")
+
+    # Remove blacklisted elements
+    remove_blacklist(soup)
+
+    # Flatten elements where we want to keep the text but drop the containing tag
+    flatten_elements(soup)
+    
+    # Replace <br> and <hr> elements
+    reformat_linebreaks(soup)
+
+    # Consolidate text 
+    consolidate_text(soup)
+
+    # Normalise all strings
+    normalise_strings(soup)
+
+    # Wrap any remaining bare text in <p> tags
+    wrap_bare_text(soup)
+
+    # Finally wrap the whole tree in a div
+    root = soup.new_tag("div")
+    root.append(soup)
+    return root
+
+
+
+
+# if __name__ == "__main__":
+#     html="""
+#         <article>
+#         <header>
+#             <h2>Lorem ipsum dolor sit amet</h2>
+#             <p>Consectetur adipiscing elit</p>
+#         </header>
+#         <p>Vestibulum leo nulla, imperdiet a pellentesque ultrices aliquam.</p>
+#         <button type="button">Click Me!</button>
+#         </article>
+#         <datalist id=sexes>
+#             <option value="Female">
+#             <option value="Male">
+#         </datalist>
+#         <span>Some text here</span>
+#         <p class="something">Some extra text, single-broken,<br/> that is split with double <br/><br/> line breaks in such a way that
+#         it is wrapped<hr/>and has horizontal rules as well as <br/><br/><br/> triple
+#         line breaks</p>
+
+#         <a href="whatever">here are <cite>nested</cite> flat elements</a>
+#         <p>Here is
+#         a paragraph
+#         with non-syntactical line-breaks
+#         </p>
+
+#         And finish with some bare
+#         text that has linebreaks
+#         in odd places
+
+#         Another piece of text
+#         <br>
+#         <br>
+#         A new paragaph
+#         <br>
+#         I should be merged with the previous paragraph
+#     """
+#     parsed_html = parse_html(html, node_indexes=True)
+#     print(parsed_html.prettify())
--- readability.py
+++ readability.py
@@ -6,24 +6,31 @@ import os
 from subprocess import check_call
 import tempfile
 import unicodedata
+from .plain_html import parse_to_tree
 
 
-def parse(html, content_digests=False, node_indexes=False):
-    temp_dir = tempfile.gettempdir()
-    # Write input HTML to temporary file so it is available to the node.js script
-    html_path = os.path.join(temp_dir, "full.html");
-    with open(html_path, 'w') as f:
-        f.write(html)
+def parse(html, content_digests=False, node_indexes=False, use_readability=False):
+    if use_readability:
+        temp_dir = tempfile.gettempdir()
+        # Write input HTML to temporary file so it is available to the node.js script
+        html_path = os.path.join(temp_dir, "full.html")
+        with open(html_path, 'w') as f:
+            f.write(html)
 
-    # Call Mozilla's Readability.js Readability.parse() function via node, writing output to a temporary file
-    article_json_path = os.path.join(temp_dir, "article.json")
-    parse_script_path = os.path.join(os.path.dirname(__file__), "ExtractArticle.js")
-    check_call(["node", parse_script_path, "-i", html_path, "-o", article_json_path])
+        # Call Mozilla's Readability.js Readability.parse() function via node, writing output to a temporary file
+        article_json_path = os.path.join(temp_dir, "article.json")
+        parse_script_path = os.path.join(os.path.dirname(__file__), "ExtractArticle.js")
+        check_call(["node", parse_script_path, "-i", html_path, "-o", article_json_path])
 
-    # Read output of call to Readability.parse() from JSON file and return as Python dictionary
-    with open(article_json_path) as f:
-        readability_json = json.loads(f.read())
+        # Read output of call to Readability.parse() from JSON file and return as Python dictionary
+        with open(article_json_path) as f:
+            input_json = json.loads(f.read())
 
+    else:
+        input_json = {
+            "content": str(parse_to_tree(html))
+        }
+    
     # Only keep the subset of Readability.js fields we are using (and therefore testing for accuracy of extraction)
     # TODO: Add tests for additional fields and include them when we look at packaging this wrapper up for PyPI
     # Initialise output article to include all fields with null values
@@ -35,13 +42,13 @@ def parse(html, content_digests=False, node_indexes=False):
         "plain_text": None
     }
     # Populate article fields from readability fields where present
-    if readability_json:
-        if "title" in readability_json and readability_json["title"] is not "":
-            article_json["title"] = readability_json["title"]
-        if "byline" in readability_json and readability_json["byline"] is not "":
-            article_json["byline"] = readability_json["byline"]
-        if "content" in readability_json and readability_json["content"] is not "":
-            article_json["content"] = readability_json["content"]
+    if input_json:
+        if "title" in input_json and input_json["title"] is not "":
+            article_json["title"] = input_json["title"]
+        if "byline" in input_json and input_json["byline"] is not "":
+            article_json["byline"] = input_json["byline"]
+        if "content" in input_json and input_json["content"] is not "":
+            article_json["content"] = input_json["content"]
             article_json["plain_content"] = \
                 plain_content(article_json["content"], content_digests, node_indexes)
             article_json["plain_text"] = \
