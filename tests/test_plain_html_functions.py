a9861be770344cefe1bf7ac2c6ca2afafa2cce05
James Robinson <james.em.robinson@gmail.com>
2018-12-14 12:28:02 +0000

    Added initial tests for functions called in plain_html.

---
+++ tests/test_plain_html_functions.py
@@ -0,0 +1,31 @@
+"""Tests for HTML elements."""
+from bs4 import BeautifulSoup
+from ReadabiliPy import plain_html
+
+def test_remove_metadata():
+    html = """
+        <!DOCTYPE html>
+        <html>
+        <head></head>
+        <body>
+        <!-- Comment here -->
+        </body>
+        </html>
+    """
+    soup = BeautifulSoup(html, "html5lib")
+    plain_html.remove_metadata(soup)
+    assert "<!-- Comment here -->" not in str(soup)
+
+
+def test_remove_blacklist():
+    html = """
+        <html>
+        <body>
+            <button type="button">Click Me!</button>
+            <p>Hello</p>
+        <body>
+        </html>
+    """
+    soup = BeautifulSoup(html, "html5lib")
+    plain_html.remove_blacklist(soup)
+    assert "button" not in str(soup)
