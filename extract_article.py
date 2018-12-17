06cc6e1c9918cd7092b212cda3cfcb273a82ca98
James Robinson <james.em.robinson@gmail.com>
2018-12-17 10:12:52 +0000

    Expanded description string

--- extract_article.py
+++ extract_article.py
@@ -5,17 +5,15 @@ import readability
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Extract article data from a HTML file using Mozilla's Readability.js package.")
+        description="Extract article data from a HTML file using either Mozilla's Readability.js package or a simplified python-only alternative.")
     parser.add_argument('-i', '--input-file', required=True,
                         help='Path to input file containing HTML.')
     parser.add_argument('-o', '--output-file', required=True,
                         help='Path to file to output the article data to as JSON.')
     parser.add_argument('-c', '--content-digests', action='store_true',
-                        help='Add a "data-content-digest" attribute containing a SHA256-based digest of the '
-                             'element\'s contents to each HTML element in the plain_content output.')
+                        help='Add a "data-content-digest" attribute containing a SHA256-based digest of the element\'s contents to each HTML element in the plain_content output.')
     parser.add_argument('-n', '--node-indexes', action='store_true',
-                        help='Add a "data-node-index" attribute containing a hierarchical representation of the '
-                             'element\'s position in the HTML structure each HTML element in the plain_content output.')
+                        help='Add a "data-node-index" attribute containing a hierarchical representation of the element\'s position in the HTML structure each HTML element in the plain_content output.')
 
     args = parser.parse_args()
 
