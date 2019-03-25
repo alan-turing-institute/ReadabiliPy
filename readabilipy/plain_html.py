be66647540abed541ee6345bac07b1458cf19197
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2019-03-25 10:30:28 +0000

    extract_element now checks all possible tags, not just the first

--- readabilipy/plain_html.py
+++ readabilipy/plain_html.py
@@ -368,23 +368,30 @@ def extract_element(html, extraction_paths):
                             # Only for the lowest level element do we filter by attribute
                             soup_tag = soup.find(path[0])
                             i = 1
+                            soup_tags = []
                             for level in path[1:]:
                                 i += 1
                                 if soup_tag:
                                     if i == len(path):
-                                        soup_tag = soup_tag.find(path, attr_set)
+                                        soup_tags.append(soup_tag.find(path, attr_set))
                                     else:
-                                        soup_tag = soup_tag.find(path)
+                                        soup_tags.append(soup_tag.find(path))
                         else:
-                            soup_tag = soup.find(path[0], attr_set)
+                            soup_tags = soup.find_all(path[0], attr_set)
 
                         # Handle the element being text in the HTML or an attr (e.g. content)
-                        if tag_dict["element"] == "text":
-                            if soup_tag and soup_tag.text:
-                                element = soup_tag.text
-                        else:
-                            if soup_tag and soup_tag.has_attr(tag_dict["element"]):
-                                element = soup_tag[tag_dict["element"]]
+                        # If soup_tag was not found, element does not get set
+                        goToNextTag = True
+                        for soup_tag in soup_tags:
+                            if goToNextTag:
+                                if tag_dict["element"] == "text":
+                                    if soup_tag and soup_tag.text:
+                                        element = soup_tag.text
+                                        goToNextTag = False
+                                else:
+                                    if soup_tag and soup_tag.has_attr(tag_dict["element"]):
+                                        element = soup_tag[tag_dict["element"]]
+                                        goToNextTag = False
 
     # Strip unwanted spaces from the element
     if element:
