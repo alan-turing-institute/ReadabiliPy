59718ec14ecde59cdd37f2609bb0fc14a08d71e9
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2019-03-28 16:50:59 +0000

    refactor

--- readabilipy/extractors/extract_element.py
+++ readabilipy/extractors/extract_element.py
@@ -1,7 +1,6 @@
-from bs4 import BeautifulSoup
-from ..text_manipulation import normalise_whitespace
-from ..plain_html import unwrap_elements
+from collections import defaultdict
 import lxml.html
+from ..text_manipulation import normalise_whitespace
 
 
 def extract_element(html, xpaths):
@@ -9,27 +8,18 @@ def extract_element(html, xpaths):
         xpaths should be a list of tuples, each with the xpath and a reliability score
     """
 
-    # Discard certain html elements while keeping contents
-    soup = BeautifulSoup(html)
-    unwrap_elements(soup)
-    html = soup.prettify()
-
     # Parse the html and extract the contents using all xpaths
     lxml_html = lxml.html.fromstring(html)
-    elements = []
-    element_tuples = []
+    extracted_strings = defaultdict(int)
+
+    # Get all elements specified and concatenate scores
     for extraction_xpath, score in xpaths:
         for found_element in lxml_html.xpath(extraction_xpath):
             element = normalise_whitespace(found_element)
-            element_tuples.append((element, score))
-            elements.append(element)
+            if element:
+                extracted_strings[element] += score
 
-    if len(elements) == 0:
-        return None  # Return None when no xpath elements in the HTML
-    else:
-        # Returns the most common element by default
-        # Returns the most reliable (highest score) element when elements equally common
-        if len(elements) == len(set(elements)):  # if all elements unique
-            return max(element_tuples, key=lambda item: item[1])[0]
-        else:
-            return max(set(elements), key=elements.count)
+    # Return highest scoring element
+    if not extracted_strings:
+        return None
+    return max(extracted_strings, key=extracted_strings.get)
