7133d7b4f54575322818b1667e8a21d9359a8570
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2019-04-10 14:27:17 +0100

    refactor to 2 functions

--- readabilipy/extractors/extract_element.py
+++ readabilipy/extractors/extract_element.py
@@ -3,10 +3,7 @@ import lxml.html
 from ..text_manipulation import normalise_whitespace
 
 
-def extract_element(html, xpaths, delete_longer=True):
-    """Return the relevant element (title, date or byline) from article HTML
-        xpaths should be a list of tuples, each with the xpath and a reliability score
-    """
+def get_element_candidates(html, xpaths, score_lower_limit=0):
 
     # Parse the html and extract the contents using all xpaths
     lxml_html = lxml.html.fromstring(html)
@@ -14,7 +11,7 @@ def extract_element(html, xpaths, delete_longer=True):
 
     # Get all elements specified and concatenate scores
     for extraction_xpath, score in xpaths:
-        if score > 0:
+        if score >= score_lower_limit:
             found_elements = lxml_html.xpath(extraction_xpath)
             if found_elements:
                 # If just one found
@@ -29,6 +26,15 @@ def extract_element(html, xpaths, delete_longer=True):
                             element = normalise_whitespace(found_element)
                             if element:
                                 extracted_strings[element] += score
+    return extracted_strings
+
+
+def extract_element(html, xpaths, delete_longer=True):
+    """Return the relevant element (title, date or byline) from article HTML
+        xpaths should be a list of tuples, each with the xpath and a reliability score
+    """
+
+    extracted_strings = get_element_candidates(html, xpaths, score_lower_limit=0)
 
     # Consider elements where one is just a longer version of another to be the same and concatenate scores
     delete_these = []
@@ -45,24 +51,10 @@ def extract_element(html, xpaths, delete_longer=True):
         if del_str in extracted_strings:
             del extracted_strings[del_str]
 
+    # Only search with xpaths that have a score of -1 if nothing else can be found
     if not extracted_strings:
-        for extraction_xpath, score in xpaths:
-            if score < 0:
-                found_elements = lxml_html.xpath(extraction_xpath)
-                if found_elements:
-                    # If just one found
-                    if isinstance(found_elements, lxml.etree._ElementUnicodeResult):
-                        element = normalise_whitespace(found_elements)
-                        if element:
-                            extracted_strings[element] += score
-                    # If a list of elements found:
-                    else:
-                        for found_element in found_elements:
-                            if isinstance(found_element, lxml.etree._ElementUnicodeResult):
-                                element = normalise_whitespace(found_element)
-                                if element:
-                                    extracted_strings[element] += score
-    
+        extracted_strings = get_element_candidates(html, xpaths, score_lower_limit=-1)
+
     # Return highest scoring element
     if not extracted_strings:
         return None
