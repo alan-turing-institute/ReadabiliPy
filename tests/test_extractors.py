cc073e48d07e01242026168a70363093678a7e23
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2019-04-16 11:07:57 +0100

    add test for h:m A MM/DD/YYYY format

--- tests/test_extractors.py
+++ tests/test_extractors.py
@@ -74,6 +74,7 @@ def test_extract_date():
         ("""<p class="text-muted">Posted Friday, October 19, 2018</p>""", "2018-10-19T00:00:00"),
         ("""<div class="text"><p>2019-01-25T15:16:00+00:00</p></div>""", "2019-01-25T15:16:00"),
         ("""<span class="timestamp " data-localize-time data-epoch-time="1545342875000" data-time-zone="-0800" data-time-format="%A %B %d, %Y">Thursday December 20, 2018</span>""", "2018-12-20T00:00:00"),
+        ("""<time style="text-transform:uppercase">8:16 AM 01/31/2019 | Politics</time>""", "2019-01-31T08:16:00"),
         ("""
             <div class="article-byline">By
                 <span itemprop="author creator" itemtype="http://schema.org/Person" itemid="/by/michael-gryboski">
