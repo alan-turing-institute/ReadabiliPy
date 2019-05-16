eecaa5d0ffa46912235f1268be9a7da4718759fb
James Robinson <james.em.robinson@gmail.com>
2019-05-16 12:09:02 +0100

    Updated date extraction logic

--- readabilipy/extractors/extract_date.py
+++ readabilipy/extractors/extract_date.py
@@ -9,12 +9,12 @@ def extract_date(html):
     # Tuple scores reflect confidence in these xpaths and the preference used for extraction
     xpaths = [
         ('//meta[@property="article:published_time"]/@content', 13),
-        ('//meta[@property="article:modified_time"]/@content', 2),
-        ('//meta[@property="article:published"]/@content', 7),
         ('//meta[@property="og:updated_time"]/@content', 10),
-        ('//meta[@itemprop="dateModified"]/@content', 2),
+        ('//meta[@property="article:published"]/@content', 7),
         ('//meta[@itemprop="datePublished"]/@content', 3),
         ('//time/@datetime', 3),
+        ('//meta[@itemprop="dateModified"]/@content', 2),
+        ('//meta[@property="article:modified_time"]/@content', 2),
     ]
 
     # Get all the dates
@@ -22,24 +22,28 @@ def extract_date(html):
     if len(extracted_dates) == 0:
         return None
     # Set the date_string as that with the highest score assigned by extract_element
-    date_string = max(extracted_dates, key=lambda x: extracted_dates[x].get('score'))
+    date_string = max(extracted_dates, key=lambda x: extracted_dates[x]["score"])
     return ensure_iso_date_format(date_string)
 
 
 def ensure_iso_date_format(date_string, ignoretz=True):
     """Check date_string is in one of our supported formats and return it"""
     supported_date_formats = [
-        "%Y-%m-%dT%H:%M:%S",  # e.g. '2014-10-24T17:32:46'
-        "%Y-%m-%dT%H:%M:%S%z",  # e.g. '2014-10-24T17:32:46+12:00'
-        "%Y-%m-%dT%H:%M:%SZ",  # e.g. '2019-02-18T17:52:10Z'
-        "%Y-%m-%dT%H:%M:%S.%fZ"  # e.g. '2019-02-15T15:54:50.000Z'
+        "%Y-%m-%dT%H:%M:%S",     # '2014-10-24T17:32:46'
+        "%Y-%m-%dT%H:%M:%S%z",   # '2014-10-24T17:32:46+12:00'
+        "%Y-%m-%dT%H:%M:%SZ",    # '2014-10-24T17:32:46Z'
+        "%Y-%m-%dT%H:%M:%S.%fZ"  # '2014-10-24T17:32:46.000Z'
     ]
 
     for date_format in supported_date_formats:
-        try:  # Below required for Python versions < 3.7 (works with >= 3.7), removes colon from timezone e.g. '2014-10-24T17:32:46+12:00' => '2014-10-24T17:32:46+1200'
+        try:
+            # For python < 3.7, strptime() is not able to parse timezones containing
+            # colons (eg. 2014-10-24T17:32:46+12:00). By stripping the colon here,
+            # we ensure that all versions of python can parse datetimes like these
             if date_format == "%Y-%m-%dT%H:%M:%S%z" and date_string[-3] == ':':
-                date_string = date_string[:-3] + date_string[-2:]  # Remove colon between hours and minutes of timezone
-            isodate = datetime.strptime(date_string, date_format)
+                isodate = datetime.strptime(date_string[:-3] + date_string[-2:], date_format)
+            else
+                isodate = datetime.strptime(date_string, date_format)
             if ignoretz:
                 isodate = isodate.replace(tzinfo=None)
             return isodate.isoformat()
