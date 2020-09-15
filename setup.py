55e30ff8a7c87d88a0d6f8f3ce868251a36bcd9c
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2020-09-15 12:03:10 +0100

    Set version to None in setup.py
    This ensures that it is pulled from the __version__ file.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/alan-turing-institute/ReadabiliPy"
 EMAIL = "jrobinson@turing.ac.uk"
 AUTHOR = "The Alan Turing Institute"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.1.0"
+VERSION = None
 
 # What packages are required for this module to be executed?
 REQUIRED = [
